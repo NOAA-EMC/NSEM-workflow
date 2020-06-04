184ddfeaa3da169d4f74de62e4d1a1f7552e7aa2
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-06-04 15:55:46 +0000

    Updated documentation

--- ush/nsem_utils.py
+++ ush/nsem_utils.py
@@ -1,3 +1,26 @@
+# ----------------------------------------------------------- 
+# Python Script File
+# Tested Operating System(s): RHEL 7, Python 3.7.5
+# Tested Run Level(s): 
+# Shell Used: BASH shell
+# Original Author(s): Saeed Moghimi
+# File Creation Date: 05/15/2020
+# Date Last Modified:
+#
+# Version control: 1.00
+#
+# Support Team:
+#
+# Contributors: Andre van der Westhuysen
+#
+# ----------------------------------------------------------- 
+# ------------- Program Description and Details ------------- 
+# ----------------------------------------------------------- 
+#
+# Python utility module
+#
+# -----------------------------------------------------------
+
 from string import Template
 
 def tmp2scr(filename,tmpname,d):
