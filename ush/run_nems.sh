f01d60333d1ac71670f757dbd855fa452b7f49ea
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-09-28 16:49:35 +0000

    Added WW3 restart file copy

--- ush/run_nems.sh
+++ ush/run_nems.sh
@@ -27,9 +27,11 @@ echo "=== In run_nems.sh ==="
 cd ${RUNdir}
 
 echo "Running NEMS.x..."
+#srun -v --slurmd-debug=4 --mem=3000 ./NEMS.x
 srun ./NEMS.x
 
 echo "Copying restart files to GESOUT..."
 mkdir ${GESOUT}/hotfiles
 cp -p fort.67.nc ${GESOUT}/hotfiles/
 cp -p fort.68.nc ${GESOUT}/hotfiles/
+cp -p restart*.* ${GESOUT}/hotfiles/
